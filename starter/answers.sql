18e35b3b12e6f8ef6a32ca5d66e587218465baaa
SATHISHKUMAR P <sathishkumar99611@gmail.com>
2026-09-25 12:50:21 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,1 +1,15 @@
+CREATE TABLE Student
+(
+StudentID INT(5) PRIMARY KEY,
+StudentName VARCHAR(20) NOT NULL,
+DOB DATE DEFAULT NULL,
+Gender VARCHAR(10) NOT NULL,
+DepartmentID INT(5),
+CONSTRAINT UQ_StudentName UNIQUE (StudentName),
+CONSTRAINT FK_Student_Department
+FOREIGN KEY (DepartmentID)
+REFERENCES Department(DepartmentID)
+);
+DESC Student;
+DROP TABLE Student;
 
